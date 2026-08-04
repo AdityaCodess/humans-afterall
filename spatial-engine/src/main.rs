f26e8606fa299540902d5ad2109d406fab12c11c
ADITYA BHALLA <adityabhalla2007@gmail.com>
2026-08-05 00:39:34 +0530

    dockerized

--- spatial-engine/src/main.rs
+++ spatial-engine/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    println!("Spatial Engine booted up and listening on port 9000...");
+    
+    // Continuous loop keeps the Rust process alive
+    loop {
+        sleep(Duration::from_secs(1)).await;
+    }
 }
